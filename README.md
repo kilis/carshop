143572500dd742d594d3919f08fd2e227a3204fc
Kritaps <killislv@gmail.com>
2016-03-14 07:22:40 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # carshop
 ![alt tag](https://i.imgur.com/7lZwLKc.jpg)
+Pirmaa termiņa pirmkods 
--- resources/views/admin/new.blade.php
+++ resources/views/admin/new.blade.php
@@ -26,7 +26,7 @@
 					<div class="form-group">
                         <label class="col-md-3 control-label" for="sku">SKU</label>
                         <div class="col-md-9">
-                            <input id="sku" name="sku" type="text" placeholder="Product SKU" class="form-control input-md" required="">
+                            <input id="sku" name="sku" type="integer" placeholder="Product SKU" class="form-control input-md" required="">
  
                         </div>
                     </div>
